8f1b1d889d5c0935cdf1abd869efc23831823aa8
botantantan <fritz_gerald@live.com>
2021-09-27 17:14:39 +0700

    add data_worthcheck

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from preprocess import *
 
-df_train = pd.read_csv('./data_worthcheck/data_worthcheck/train.csv')
-df_test = pd.read_csv('./data_worthcheck/data_worthcheck/test.csv')
+df_train = pd.read_csv('data/train.csv')
+df_test = pd.read_csv('data/test.csv')
 
 preprocess = Preprocess(df_train, 'text_a', 'label')
 print(preprocess.data.head())
