d31639f422564108f9b54723054d0fca281118a2
dimaslucky <13518003@std.stei.itb.ac.id>
2021-09-28 13:39:27 +0700

    fix shallow

--- shallow.py
+++ shallow.py
@@ -1,5 +1,6 @@
 from sklearn import feature_extraction, feature_selection, naive_bayes, pipeline, metrics
 import pandas as pd
+import numpy as np
 
 class Shallow:
     def __init__(self, dataTrain, dataTest):
@@ -7,23 +8,26 @@ class Shallow:
         self.dataTest = dataTest
 
         self.addCleanText()
+        # print(self.dataTrain.head())
         self.extractFeature()
         self.selectFeature()
         self.updateVectorizer()
         self.trainData()
+        self.getMetrics()
 
     def addCleanText(self):
         text_clean = []
         for list_tokens in self.dataTrain['StemmedToken']:
-            temp = ''
-            temp = ' '.join(list_tokens)
+            # print(list_tokens.replace("[","").replace("'","").replace(","," ").replace("]",""))
+            temp = list_tokens.replace("[","").replace("'","").replace(","," ").replace("]","")
+            # temp = ' '.join(tokens)
             text_clean.append(temp)
         self.dataTrain["text_clean"] = text_clean
 
         text_clean = []
         for list_tokens in self.dataTest['StemmedToken']:
-            temp = ''
-            temp = ' '.join(list_tokens)
+            temp = list_tokens.replace("[","").replace("'","").replace(","," ").replace("]","")
+            # temp = ' '.join(tokens)
             text_clean.append(temp)
         self.dataTest["text_clean"] = text_clean
 
@@ -41,15 +45,15 @@ class Shallow:
         self.df_features = pd.DataFrame()
 
         for cat in np.unique(y):
-            chi2, p = feature_selection.chi2(X_train, y==cat)
-            df_features = df_features.append(pd.DataFrame(
-                        {"feature":X_names, "score":1-p, "y":cat}))
-            df_features = df_features.sort_values(["y","score"], 
+            chi2, p = feature_selection.chi2(self.X_train, y==cat)
+            self.df_features = self.df_features.append(pd.DataFrame(
+                        {"feature":self.X_names, "score":1-p, "y":cat}))
+            self.df_features = self.df_features.sort_values(["y","score"], 
                             ascending=[True,False])
-            df_features = df_features[df_features["score"]>p_value_limit]
+            self.df_features = self.df_features[self.df_features["score"]>p_value_limit]
     
     def updateVectorizer(self):
-        self.vectorizer = feature_extraction.text.TfidfVectorizer(vocabulary=X_names)
+        self.vectorizer = feature_extraction.text.TfidfVectorizer(vocabulary=self.X_names)
 
         self.vectorizer.fit(self.corpus)
         self.X_train = self.vectorizer.transform(self.corpus)
@@ -69,8 +73,8 @@ class Shallow:
         ## test
         self.X_test = self.dataTest["text_clean"].values
         self.y_test = self.dataTest['label']
-        self.predicted = model.predict(X_test)
-        self.predicted_prob = model.predict_proba(X_test)
+        self.predicted = self.model.predict(self.X_test)
+        self.predicted_prob = self.model.predict_proba(self.X_test)
 
     def getMetrics(self):
         classes = np.unique(self.y_test)
