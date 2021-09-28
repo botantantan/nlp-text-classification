5e360321c403dee8ca4ae956f40915cca034150f
dimaslucky <13518003@std.stei.itb.ac.id>
2021-09-28 17:30:42 +0700

    word embedding fix

--- main.py
+++ main.py
@@ -14,7 +14,19 @@ from word_embedding import *
 # preprocessTest = Preprocess(df_test, 'text_a', 'label', 'test')
 # print(preprocessTrain.data.head())
 
+print("METODE:")
+print("Shallow Learning (1)")
+print("Word Embedding (2)")
+print("=========================")
+print("Masukkan input sesuai nomor metode yang diinginkan")
+method = int(input())
+
 preprocessTrain = pd.read_csv('df_train_preprocessed.csv')
 preprocessTest = pd.read_csv('df_test_preprocessed.csv')
-shallow = Shallow(preprocessTrain, preprocessTest)
-wordEmbbeding = WordEmbbeding(preprocessTrain, preprocessTest)
+
+if(method == 1):
+    shallow = Shallow(preprocessTrain, preprocessTest)
+    shallow.getMetrics()
+elif(method == 2):
+    wordEmbbeding = WordEmbbeding(preprocessTrain, preprocessTest)
+    wordEmbbeding.getMetric()
